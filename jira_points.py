20f42e1dd4a15db35067c642b5009e6fc6a177dc
Israel Zalmanov <israel@eyeviewdigital.com>
2016-01-06 22:32:34 -0500

    inital commit

---
+++ jira_points.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from jira import JIRA
+from itertools import groupby
+
+user_name=""
+pasword=""
+project="Data"
+
+jira = JIRA("https://eyeview.atlassian.net",basic_auth=(user_name, pasword))
+issues = jira.search_issues('project=%s and sprint in openSprints ()' % project)
+x=[(i.fields.status.name, i.fields.customfield_10004) for i in issues]
+x.sort()
+y=groupby(x,key = lambda x:x[0])
+for key, rows in y:
+    print key, sum(r[1] for r in rows if r[1] is not None)
